b05ae1585472e43bb25c46181f4b639211c916a3
spike1140 <123886310+spike1140@users.noreply.github.com>
2023-01-30 12:07:05 -0300

    inicial commit

---
+++ function.js
@@ -0,0 +1,36 @@
+       
+       document.getElementById('1show').onclick = function () {
+       console.log("prueba");
+       document.getElementById("name").innerHTML = "Raymond Little" ;
+      };
+
+      document.getElementById('2show').onclick = function () {
+        console.log("prueba2");
+        document.getElementById("mail").innerHTML = "raymond.little@example.com" ;
+       };
+      
+       document.getElementById('3show').onclick = function () {
+        console.log("prueba3");
+        document.getElementById("nas").innerHTML = "5/5/1968" ;
+       };
+
+
+       document.getElementById('4show').onclick = function () {
+        console.log("prueba4");
+        document.getElementById("direc").innerHTML = "1569 Hickory Creek Dr" ;
+       };
+
+
+       document.getElementById('5show').onclick = function () {
+        console.log("prueba5");
+        document.getElementById("tel").innerHTML = "(603) 571-7191" ;
+       };
+
+
+       document.getElementById('6show').onclick = function () {
+        console.log("prueba6");
+        document.getElementById("pass").innerHTML = "guitar" ;
+       };
+
+
+     
